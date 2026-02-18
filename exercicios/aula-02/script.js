53d263c2fa31fc3d9dc7195364e18b72d0cc5abf
GoBrazill <vinicius.daraujos26@gmail.com>
2026-02-18 09:05:44 -0300

    feat: add aula 02 assigment

---
+++ exercicios/aula-02/script.js
@@ -0,0 +1,12 @@
+let ateu = "26"
+console.log(ateu);
+
+
+let AteuConvertido = Number(ateu)
+console.log(convertido);
+
+
+let agnostico = Boolean(AteuConvertido)
+console.log(convertido2);
+
+// Não sei o que tem que explicar aq :/
